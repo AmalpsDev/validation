506f4946a6838589ce9e3d6d850cf2c3f0f5b35b
AmalpsDev <frendzyframes@gmail.com>
2022-10-19 22:04:56 +0530

    AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
@@ -35,7 +36,7 @@ class AdminController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       
     }
 
     /**
@@ -58,7 +59,69 @@ class AdminController extends Controller
      */
     public function save(Request $request)
     {
-         
+          try{
+            if($request->ajax()){
+
+                DB::beginTransaction();
+                $name=$request->name;
+                $email=$request->email;
+                $password1=$request->password1;
+
+                $data=['name'=>$name,'email'=>$email,'password'=>$password1];
+                
+                $filter1=[ 
+                    'name' => 'trim|escape',
+                    'password'=>'trim|escape',
+                    'email'=>'trim|escape',
+                  ];
+
+                  $rules1 = array(
+                    'name' => 'Required',
+                    'password' => 'Required',
+                    'email' => 'Required',
+                    );
+
+                    $messages1 = [];
+                    $validator1 = Validator::make($data, $rules1, $messages1);
+
+                    if($validator1->fails())
+                    {
+                        $errors = $validator1->errors()->toArray();
+                        $data = array('error'=>$errors);
+                        echo json_encode($data);
+                        exit();
+                    }else{
+                       
+                        $data_exists = DB::table('creation.user_details')
+                                    ->where('email',$email)
+                                    ->exists();
+                        if($data_exists==false){
+                                $ins=DB::table('creation.user_details')
+                                        ->insertGetId($data,'id');
+                                        if($ins){
+                                             DB::commit();
+                                            echo json_encode(1);//data inserted successfully
+                                            exit();
+                                        }else{
+                                             DB::rollback();
+                                            echo json_encode(2);//data inserted successfully
+                                            exit();
+                                        }
+                                           
+
+                        }else{
+                            echo json_encode(3); 
+                            exit(); //Email id Exists
+                        }            
+                    }
+
+            }else{
+                return abort(404);
+            }
+
+        }catch(Exception $e){
+            return abort(404);
+        }
     }
 
     /**
