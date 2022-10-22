0e7472c4a0b96e9707bf86d1d6b6073993cdc710
AmalpsDev <frendzyframes@gmail.com>
2022-10-22 21:53:23 +0530

    AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Exception;
 
 class AdminController extends Controller
 {
