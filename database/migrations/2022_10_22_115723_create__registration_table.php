ae3bc02b81fc5396340dc0b75c4a071dde93ce55
AmalpsDev <frendzyframes@gmail.com>
2022-10-22 21:41:20 +0530

    create registration table

---
+++ database/migrations/2022_10_22_115723_create__registration_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->date('data_of_birth');
+            $table->string('email',50);
+            $table->date('registration_date');
+            $table->bigInteger('registration_number');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_registration');
+    }
+}
